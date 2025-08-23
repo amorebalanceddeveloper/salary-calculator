197dcdfc2a0b561b2ee4c4727caf6c61c85a914d
amorebalanceddeveloper <kqcareer@outlook.com>
2025-08-23 11:43:16 -0400

    Update calculator.js
    Variables for time variables

--- calculator.js
+++ calculator.js
@@ -2,4 +2,10 @@ function calculate() {
   let salary;
   let timeRate;
   let finalSalaryAmount;
+  let weekly = 7;
+  let biweekly = 14;
+  let weekCount = 52;
+  let monthly30Days = 30;
+  let monthly31Days = 31;
+  let yearMonthCount = 12;
 }
